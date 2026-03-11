3e575f56d3b24eab8d97aee0069714faccd1297f
BNsaicharan17 <saic48894@gmail.com>
2026-03-11 19:48:18 +0530

    Remove unused TensorFlow import
    Removed unused TensorFlow import to clean up code.

--- image_dl_model.py
+++ image_dl_model.py
@@ -7,7 +7,7 @@ Features:
 """
 
 import numpy as np
-import tensorflow as tf
+
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing.image import img_to_array
